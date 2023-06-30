98f5fb6123cd8ad943e2743fc7d4e0eb0fe27cd8
Kwanele <kwanele@Kwaneles-MacBook-Pro.local>
2023-06-30 14:24:10 -0600

    styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,20 +3,39 @@ import { Row, Col } from "react-bootstrap"
 // import SideNav from "./sideNav"
 
 const Header = (props) => {
-  const {className, headerText, subText, headerImg} = props
+  
+  const { className, headerText, subText, headerImg } = props
+  
+  const styles = {
+    backgroundImage: `${headerImg}`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center"
+  }
+
   return (
     <>
-      <Row className={className} style={ { backgroundImage: `${headerImg}` }}>
+      <Row className={className} style={styles}>
         <Col md="12">
           <NavMenu />
         </Col>
-        <Col className="text-center h-25" style={{zIndex: 10}}>
-          <h1 className="text-center text-light"> {headerText} </h1>
-          <p className="text-center text-light col-md-4 mx-auto mt-4" style={{fontSize: "20px"}}> {subText} </p>
+        <Col sm={12} className="text-center" style={{ zIndex: 10 }}>
+          <Row>
+            <Col sm={12}>
+              <h1 className="text-center text-light w-75 mx-auto"> {headerText} </h1>
+            </Col>
+            <Col>
+              <p className="text-center text-light col-md-4 mx-auto" style={{ fontSize: "20px" }}>
+                {subText}{" "}
+              </p>
+            </Col>
+          </Row>
         </Col>
       </Row>
     </>
   )
 }
+
+
 
 export default Header
